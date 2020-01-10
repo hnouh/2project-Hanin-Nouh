e9315d63e0de66a008dda07b9639be329701151b
Hanin Nouh <haninnouh@gmail.com>
2020-01-10 13:27:16 +0300

    type to category

--- db/migrate/20191220234151_create_animes.rb
+++ db/migrate/20191220234151_create_animes.rb
@@ -2,7 +2,7 @@ class CreateAnimes < ActiveRecord::Migration[6.0]
   def change
     create_table :animes do |t|
       t.string :name
-      t.string :type
+      t.string :category
       t.integer :aired_year
       t.integer :episodes
       t.integer :duration
